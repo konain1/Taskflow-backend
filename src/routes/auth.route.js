ac2967b3bf2ac701ab54df9aba18ebfd7def3e46
Kaunain kasab <kaunainkasab@Kaunains-MacBook-Air.local>
2026-07-14 04:50:59 +0530

    feat: protect middlewared has been created also tested : passed

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,12 @@
 const { register, login } = require("../controllers/auth.controller");
+const protect = require("../middlewares/auth.middleware");
 
 const routes = (app) => {
     app.post('/taskflow/api/v1/register', register);
     app.post('/taskflow/api/v1/login', login);
+    app.get('/taskflow/api/v1/test', protect, (req, res) => {
+        res.status(200).json({ success: true, message: "Authenticated!", user: req.user });
+    });
 }
 
 module.exports = routes;
