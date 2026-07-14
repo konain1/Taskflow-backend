b07288498919cfbb87d5b4214deba4c31d2d0753
Kaunain kasab <kaunainkasab@Kaunains-MacBook-Air.local>
2026-07-14 09:51:07 +0530

    fix : delete permission fixed now owner and admin can delete project

--- src/routes/project.route.js
+++ src/routes/project.route.js
@@ -4,7 +4,7 @@ const authorize = require('../middlewares/authorise.middleware')
 const routes = (app) => {
 
     app.post('/taskflow/api/v1/create', protect, createProject)
-    app.delete('/taskflow/api/v1/test/project/delete/:id', protect,authorize("admin"),deleteProject,(req, res) => {
+    app.delete('/taskflow/api/v1/test/project/delete/:id', protect,authorize("admin","owner"),deleteProject,(req, res) => {
             res.status(200).json({ success: true, message: "project created", user: req.user });
         });
     
